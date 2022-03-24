0ce1f8845136a743f89c2dfb76dc92c03f1e8950
yirzaraji <not.iremy@gmail.com>
2022-03-25 00:06:20 +0100

    boostrap

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomePageController extends AbstractController
 {
     /**
      * @Route("/contact", name="app_home_page")
      */
-    public function index(): Response
+    public function home(Request $request): Response
     {
-        return $this->render('home_page/index.html.twig', [
+
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash(
+                'success',
+                'Votre message a bien été <b<envoyé</b>'
+            );
+            return $this->redirectToRoute('app_home_page');
+        }
+        
+        return $this->render('index.html.twig', [
+            'contactForm' => $form->createView(),
             'controller_name' => 'HomePageController',
         ]);
     }
