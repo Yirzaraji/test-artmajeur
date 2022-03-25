5bc890303aacd11c77fb09d9a0e431ca9d0b2a9e
yirzaraji <not.iremy@gmail.com>
2022-03-26 00:19:11 +0100

    form contact persistance

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use App\Entity\Contact;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +15,24 @@ class HomePageController extends AbstractController
     /**
      * @Route("/contact", name="app_home_page")
      */
-    public function home(Request $request): Response
+    public function home(Request $request, ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
 
+        $contact_message = new Contact();
         $form = $this->createForm(ContactType::class);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->addFlash(
+
+            $contact_message = $form->getData();
+            $entityManager->persist($contact_message);
+            $entityManager->flush();
+            
+            /* $this->addFlash(
                 'success',
                 'Votre message a bien été <b<envoyé</b>'
-            );
+            ); */
             return $this->redirectToRoute('app_home_page');
         }
         
--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ContactRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ContactRepository::class)
@@ -24,6 +25,7 @@ class Contact
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email(message = "The email '{{ value }}' is not a valid email.")
      */
     private $mail;
 
